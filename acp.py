cfceaad1420e555e69ba0c32ba5d8955c80538f1
TheoTallegas <theo.tallegas@etudiant.univ-rennes.fr>
2026-09-23 14:33:05 +0200

    Ajout du calcul de l'acp

--- acp.py
+++ acp.py
@@ -13,7 +13,25 @@ data = pd.read_csv("steam.csv")
 # 3. Analyse en composante principale
 # On utilise les attributs suivants :
 # price, positives_reviews, negative_reviews, average_playtime, median_playtime, achivements, required_age
+colonnes_quantitatives = [
+    'price', 
+    'positive_ratings', 
+    'negative_ratings', 
+    'average_playtime', 
+    'median_playtime', 
+    'achievements', 
+    'required_age'
+]
 
 # Standardisation des données
+x = data[colonnes_quantitatives]
+
 temp = x.sub(x.mean())
 x_scaled = temp.div(x.std())
+
+
+# On calcule la modélisation de l'ACP
+pca = PCA(n_components=3)
+pca_res = pca.fit_transform(x_scaled)
+
+print(pca_res)
