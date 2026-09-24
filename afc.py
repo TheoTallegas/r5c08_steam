824a794c4b4ec07a0c7b66c06c86fea7cc8a761d
TheoTallegas <theo.tallegas@etudiant.univ-rennes.fr>
2026-09-24 19:16:21 +0200

    Ajout de l'AFC avec 3 rotations

--- afc.py
+++ afc.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn
 
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, FactorAnalysis
 from sklearn.preprocessing import StandardScaler
 
 from factor_analyzer import FactorAnalyzer
@@ -25,8 +25,47 @@ print("Chi square : ", chi_square_value)
 print("p : ", p_value)
 
 
+# Première analyse sans rotation
 fa = FactorAnalyzer(n_factors = 6, rotation=None)
 fa.fit(data_scaled)
 ev, v = fa.get_eigenvalues()
 print(ev)
 
+# Premier graphique
+plt.scatter(range(1,data_scaled.shape[1]+1), ev)
+plt.plot(range(1,data_scaled.shape[1]+1), ev)
+plt.title("Scree Plot")
+plt.xlabel("Factors")
+plt.ylabel("Eigenvalue")
+plt.grid()
+plt.show()
+
+
+# AFC avec 3 rotations différentes
+methods = [
+    ("FA No rotation", FactorAnalysis(2,)),
+    ("FA Varimax", FactorAnalysis(2, rotation="varimax")),
+    ("FA Quartimax", FactorAnalysis(2, rotation="quartimax")),
+]
+fig, axes = plt.subplots(ncols=3, figsize=(10, 8), sharex=True, sharey=True)
+
+for ax, (method, fa) in zip(axes, methods):
+    fa = fa.fit(data_scaled)
+
+    components = fa.components_
+
+    vmax = np.abs(components).max()
+    ax.scatter(components[0 ,:], components[1, :])
+    ax.axhline(0, -1, 1, color="k")
+    ax.axvline(0, -1, 1, color="k")
+    for i,j, z in zip(components[0, :], components[1, :], data_scaled.columns):
+        ax.text(i+.02, j+.02, str(z), ha="center")
+    for i,j, z in zip(components[0, :], components[1, :], data_scaled.index):
+        ax.text(i+.02, j+.02, str(z), ha="center")
+    ax.set_title(str(method))
+    if ax.get_subplotspec().is_first_col():
+        ax.set_ylabel("Factor 1")
+    ax.set_xlabel("Factor 2")
+
+plt.tight_layout()
+plt.show()
